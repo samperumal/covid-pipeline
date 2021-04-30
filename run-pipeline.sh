f143eaeeb558d9a19d5d856394203c50c389a640
Sam Perumal <sam_lki@yahoo.com>
2021-04-30 07:27:33 +0000

    Set *nix file permissions

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker run --rm -v sacorona_data_volume:/var/data sacorona/scrapy:latest \
-&& \
+#docker run --rm -v sacorona_data_volume:/var/data sacorona/scrapy:latest \
+#&& \
 docker run --rm -v sacorona_data_volume:/var/data sacorona/tesseract:latest \
 && \
 docker run --rm -v sacorona_data_volume:/var/data sacorona/text-clean:latest \
