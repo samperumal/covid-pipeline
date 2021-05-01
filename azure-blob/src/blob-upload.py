fc0f2fc8dfe0327446692b872bc986c738104521
Sam Perumal <sam_lki@yahoo.com>
2021-05-01 22:49:00 +0200

    Upload to web blob

--- azure-blob/src/blob-upload.py
+++ azure-blob/src/blob-upload.py
@@ -5,23 +5,23 @@ connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 
 blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
-container_name = str("covid")
+container_name = str("$web")
 
 container_client = blob_service_client.get_container_client(container_name)
 
 files = []
 for f in os.scandir("/var/data/images"):
 	if f.is_file() and f.name.lower().endswith(".png"):
-		files.append(f.path)
+		files.append(f)
 
 for f in os.scandir("/var/data"):
 	if f.is_file():
-		files.append(f.path)
+		files.append(f)
 
-for filepath in files:
-	blob_client = blob_service_client.get_blob_client(container=container_name, blob=filepath)
+for f in files:
+	blob_client = blob_service_client.get_blob_client(container=container_name, blob=f.name)
 
-	print("Uploading: " + filepath)
+	print("Uploading: " + f.path)
 
-	with open(filepath, "rb") as data:
-			blob_client.upload_blob(data, overwrite=True)
+	with open(f.path, "rb") as data:
+			blob_client.upload_blob(data, blob_type='BlockBlob', overwrite=True)
