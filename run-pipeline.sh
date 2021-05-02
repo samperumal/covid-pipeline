1d1475ce192866c6b1ca538f4a7f4c0f8162ee0e
Sam Perumal <sam_lki@yahoo.com>
2021-05-03 00:42:46 +0200

    Run secondary R file from docker

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -5,7 +5,7 @@ docker run --rm -v sacorona_data_volume:/var/data sacorona/tesseract:latest \
 && \
 docker run --rm -v sacorona_data_volume:/var/data sacorona/text-clean:latest \
 && \
-docker run --rm -v sacorona_data_volume:/var/data sacorona/text-process:latest 
+docker run --rm -v sacorona_data_volume:/var/data sacorona/text-process:latest \
 && \
 docker run --rm -v sacorona_data_volume:/var/data \
 								-e AZURE_STORAGE_CONNECTION_STRING \
