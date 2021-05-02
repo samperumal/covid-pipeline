599bb98dc89d1fba8331c8cb6d774a67ff405750
Sam Perumal <sam_lki@yahoo.com>
2021-05-02 22:42:32 +0000

    Set content type on upload

--- azure-blob/src/blob-upload.py
+++ azure-blob/src/blob-upload.py
@@ -1,5 +1,5 @@
 import os
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__, ContentSettings
 
 connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 
@@ -12,13 +12,13 @@ container_client = blob_service_client.get_container_client(container_name)
 files = []
 for f in os.scandir("/var/data/images"):
 	if f.is_file() and f.name.lower().endswith(".png"):
-		files.append([f.path, f"img/%s" % f.name])
+		files.append([f.path, f"img/%s" % f.name, "image/png"])
 
 for f in os.scandir("/var/data"):
 	if f.is_file():
-		files.append([f.path, f"data/%s" % f.name])
+		files.append([f.path, f"data/%s" % f.name, "text/" + os.path.splitext(f.name)[1][1:]])
 
-files.append(["public-html/index.html", "index.html"])
+files.append(["public-html/index.html", "index.html", "text/html"])
 
 for f in files:
 	blob_client = blob_service_client.get_blob_client(container=container_name, blob=f[1])
@@ -26,4 +26,4 @@ for f in files:
 	print("Uploading: " + f[1])
 
 	with open(f[0], "rb") as data:
-			blob_client.upload_blob(data, blob_type='BlockBlob', overwrite=True)
+			blob_client.upload_blob(data, blob_type='BlockBlob', overwrite=True, content_settings = ContentSettings(content_type=f[2]))
