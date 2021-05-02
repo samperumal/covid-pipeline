33daddbd004b94376c0a69026860872e92396350
Sam Perumal <sam_lki@yahoo.com>
2021-05-03 00:24:21 +0200

    Organise azure upload

--- azure-blob/src/blob-upload.py
+++ azure-blob/src/blob-upload.py
@@ -12,16 +12,18 @@ container_client = blob_service_client.get_container_client(container_name)
 files = []
 for f in os.scandir("/var/data/images"):
 	if f.is_file() and f.name.lower().endswith(".png"):
-		files.append(f)
+		files.append([f.path, f"img/%s" % f.name])
 
 for f in os.scandir("/var/data"):
 	if f.is_file():
-		files.append(f)
+		files.append([f.path, f"data/%s" % f.name])
+
+files.append(["public-html/index.html", "index.html"])
 
 for f in files:
-	blob_client = blob_service_client.get_blob_client(container=container_name, blob=f.name)
+	blob_client = blob_service_client.get_blob_client(container=container_name, blob=f[1])
 
-	print("Uploading: " + f.path)
+	print("Uploading: " + f[1])
 
-	with open(f.path, "rb") as data:
+	with open(f[0], "rb") as data:
 			blob_client.upload_blob(data, blob_type='BlockBlob', overwrite=True)
