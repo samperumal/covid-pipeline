a4140ac7783b1eed5cfdd037afbd9404deae50b3
Sam Perumal <sam_lki@yahoo.com>
2021-05-13 23:31:43 +0200

    Attempt to avoid 403 limitation

--- scrapy/sacoronavirus/sacoronavirus/settings.py
+++ scrapy/sacoronavirus/sacoronavirus/settings.py
@@ -14,20 +14,19 @@ NEWSPIDER_MODULE = 'sacoronavirus.spiders'
 
 LOG_LEVEL = 'DEBUG'
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'sacoronavirus (+http://www.yourdomain.com)'
+USER_AGENT = 'sacoronavirus (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
